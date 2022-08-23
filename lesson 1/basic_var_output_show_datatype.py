46c865974f208f7debf5eb5f54923c2cb6cc87c8
bobbyboy2007 <103608477+bobbyboy2007@users.noreply.github.com>
2022-08-23 15:09:10 +1200

    Add files via upload

---
+++ lesson 1/basic_var_output_show_datatype.py
@@ -0,0 +1,14 @@
+ # A S Sandhu
+ # variables 
+
+ #declaration of variables
+ name= "tiger"
+ numlegs= "4"
+ hight = "1.1"
+ hasTail = "true"
+
+ # Output to screen 
+ print("A large cat of the genus Panthera native to Africa and India is called a",Lion,'.')
+ print("An",Lion,"has",four,"legs.")
+ print("It's height is approxomately",1.1,",metres.")
+ print((hasTail))
