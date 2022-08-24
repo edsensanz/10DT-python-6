dab0b53b3c0d5fea4fe0688cd4fd23262c07fc18
bobbyboy2007 <103608477+bobbyboy2007@users.noreply.github.com>
2022-08-25 09:57:04 +1200

    Add files via upload

---
+++ lesson 1/basic_var_output_labels.py
@@ -0,0 +1,17 @@
+  # A S Sandhu
+ # variables, output, labels 
+
+ #declaration of variables
+ name = "tiger"
+ numlegs = "4"
+ hight = "1.1"
+ hasTail = "true"
+
+ # Output of variables to screen 
+ print(name)
+ print(numlegs)
+ print(hight)
+ print(hasTail)
+
+ # Output of variables with labels to screen 
+ print("The male",name,"has a large main aroud the neck of the animal.")
