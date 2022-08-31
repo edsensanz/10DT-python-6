bcd77f9792cf42d588ad14cd5d2f9d687c1f312d
bobbyboy2007 <103608477+bobbyboy2007@users.noreply.github.com>
2022-09-01 09:16:16 +1200

    Add files via upload

---
+++ lesson 1/python task 1.py
@@ -0,0 +1,23 @@
+# A S Sandhu
+# Focus - variable, labels, comments, data types.
+
+#variables
+
+
+# A summary of this information should be printed out to the screen.
+# output + priny()
+
+
+# Clarify the problem
+#name (var)
+
+
+
+#hight - expressed in metres
+
+
+#adress
+#stree umber, street name, suburb, town, city,
+
+
+#phone number
